40a88209789d51c3c519bf4e307e8ce0812984b4
M220250CS <155133820+M220250CS@users.noreply.github.com>
2024-06-18 08:47:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ from models.CQTNet import CQTNet
 
 # multi_size train
 def multi_train(**kwargs):
+    import torch
     torch.cuda.empty_cache()
+    
     parallel = True 
     opt.model = 'CQTNet'
     opt.notes='CQTNet'
@@ -45,25 +47,29 @@ def multi_train(**kwargs):
     val_data = CQT('val', out_length=None)
     test_data = CQT('test', out_length=None)
     
-    train_dataloader0 = DataLoader(train_data0, opt.batch_size, shuffle=True, num_workers=opt.num_workers, collate_fn=custom_collate_fn)
-    train_dataloader1 = DataLoader(train_data1, opt.batch_size, shuffle=True, num_workers=opt.num_workers, collate_fn=custom_collate_fn)
-    train_dataloader2 = DataLoader(train_data2, opt.batch_size, shuffle=True, num_workers=opt.num_workers, collate_fn=custom_collate_fn)
-    val_dataloader = DataLoader(val_data, 1, shuffle=False, num_workers=1, collate_fn=custom_collate_fn)
-    test_dataloader = DataLoader(test_data, 1, shuffle=False, num_workers=1, collate_fn=custom_collate_fn)
-    val_dataloader80 = DataLoader(val_data80, 1, shuffle=False, num_workers=1, collate_fn=custom_collate_fn)
+    train_dataloader0 = DataLoader(train_data0, opt.batch_size, shuffle=True, num_workers=opt.num_workers, collate_fn=custom_collate_fn, pin_memory=True, prefetch_factor=2)
+    train_dataloader1 = DataLoader(train_data1, opt.batch_size, shuffle=True, num_workers=opt.num_workers, collate_fn=custom_collate_fn, pin_memory=True, prefetch_factor=2)
+    train_dataloader2 = DataLoader(train_data2, opt.batch_size, shuffle=True, num_workers=opt.num_workers, collate_fn=custom_collate_fn, pin_memory=True, prefetch_factor=2)
+    val_dataloader = DataLoader(val_data, 1, shuffle=False, num_workers=1, collate_fn=custom_collate_fn, pin_memory=True, prefetch_factor=2)
+    test_dataloader = DataLoader(test_data, 1, shuffle=False, num_workers=1, collate_fn=custom_collate_fn, pin_memory=True, prefetch_factor=2)
+    val_dataloader80 = DataLoader(val_data80, 1, shuffle=False, num_workers=1, collate_fn=custom_collate_fn, pin_memory=True, prefetch_factor=2)
     
     # step3: criterion and optimizer
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.module.parameters() if parallel else model.parameters(), lr=opt.lr, weight_decay=opt.weight_decay)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=opt.lr_decay, patience=2, verbose=True, min_lr=5e-6)
     
+    # Gradient accumulation steps
+    accumulation_steps = 4  # Number of batches to accumulate gradients
+
     # train
     best_MAP = 0
     val_slow(model, val_dataloader80, -1)
     for epoch in range(opt.max_epoch):
         running_loss = 0
         num = 0
-        for (data0, label0), (data1, label1), (data2, label2) in tqdm(zip(train_dataloader0, train_dataloader1, train_dataloader2)):
+        optimizer.zero_grad()
+        for i, ((data0, label0), (data1, label1), (data2, label2)) in enumerate(tqdm(zip(train_dataloader0, train_dataloader1, train_dataloader2))):
             for flag in range(3):
                 if flag == 0:
                     data, label = data0, label0
@@ -71,28 +77,30 @@ def multi_train(**kwargs):
                     data, label = data1, label1
                 else:
                     data, label = data2, label2
-                
+
                 input = data.requires_grad_().to(opt.device)
                 target = label.to(opt.device)
 
-                optimizer.zero_grad()
                 score, _ = model(input)
                 loss = criterion(score, target)
                 loss.backward()
-                optimizer.step()
+
+                if (i + 1) % accumulation_steps == 0:
+                    optimizer.step()
+                    optimizer.zero_grad()
 
                 running_loss += loss.item()
                 num += target.shape[0]
-        
+
         running_loss /= num 
         print(running_loss)
         if parallel:
             model.module.save(opt.notes)
         else:
             model.save(opt.notes)
-        
-        scheduler.step(running_loss) 
-        
+
+        scheduler.step(running_loss)
+
         # validate
         MAP = 0
         MAP += val_slow(model, val_dataloader80, epoch)
